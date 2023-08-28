c2822409d5ec8ea4da6d1b1eb770214e8de65cdd
Gregory Bessonov <gbessonov@purestorage.com>
2023-08-28 08:39:38 -0600

    test

--- setup.py
+++ setup.py
@@ -12,13 +12,13 @@ NAME = 'py-pure-client'
 VERSION = '1.38.0'
 
 REQUIRES = [
-    'certifi >= 2022.9.24, <= 2022.12.7',
+    'certifi >= 2022.9.24, <= 2023.7.22',
     'six >= 1.10, <= 1.16.0',
     'python-dateutil >= 2.5.3, <= 2.8.1',
     'urllib3 >= 1.26.5, <= 1.26.12',
     'paramiko >= 2.11.0, <= 2.12.0',
-    'PyJWT >= 2.0.0, <=2.4.0',
-    'requests >= 2.20.1, <= 2.28.1',
+    'PyJWT >= 2.0.0, <=2.8.0',
+    'requests >= 2.20.1, <= 2.31.0',
     ]
 
 if sys.version_info < (3, 5):
